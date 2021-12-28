a786d2f36ff5aecfced84e5dab9bf9936f9e07b6
rizadwiandhika <rizadwiandhika@gmail.com>
2021-12-28 16:04:46 +0700

    add: admin factory

--- factory/factory.go
+++ factory/factory.go
@@ -1,1 +1,24 @@
 package factory
+
+import (
+	"github.com/final-project-alterra/hospital-management-system-api/config"
+
+	adminsBusiness "github.com/final-project-alterra/hospital-management-system-api/features/admins/business"
+	adminsData "github.com/final-project-alterra/hospital-management-system-api/features/admins/data"
+	adminsPresentation "github.com/final-project-alterra/hospital-management-system-api/features/admins/presentation"
+)
+
+type Presenter struct {
+	AdminPresentation *adminsPresentation.AdminPresentation
+}
+
+func New() *Presenter {
+	adminBuilder := adminsBusiness.NewAdminBusinessBuilder()
+
+	adminData := adminsData.NewMySQLRepo(config.DB)
+	adminPresentation := adminsPresentation.NewAdminPresentation(adminBuilder.SetData(adminData).Build())
+
+	return &Presenter{
+		AdminPresentation: adminPresentation,
+	}
+}
