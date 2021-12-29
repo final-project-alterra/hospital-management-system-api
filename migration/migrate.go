d97fa3190751145a1eab2dccb0e94759a3532c72
rizadwiandhika <rizadwiandhika@gmail.com>
2021-12-29 18:45:09 +0700

    add: doctor table migration

--- migration/migrate.go
+++ migration/migrate.go
@@ -3,12 +3,19 @@ package migration
 import (
 	"github.com/final-project-alterra/hospital-management-system-api/config"
 	adminsData "github.com/final-project-alterra/hospital-management-system-api/features/admins/data"
+	doctorsData "github.com/final-project-alterra/hospital-management-system-api/features/doctors/data"
 )
 
 func AutoMigrate() {
 	db := config.DB
 
-	err := db.AutoMigrate(&adminsData.Admin{})
+	err := db.AutoMigrate(
+		&adminsData.Admin{},
+		&doctorsData.Room{},
+		&doctorsData.Speciality{},
+		&doctorsData.Doctor{},
+	)
+
 	if err != nil {
 		panic(err)
 	}
