3cb1e128a60dbc447778f4d2cd2fce0b35eaf7cd
rizadwiandhika <rizadwiandhika@gmail.com>
2021-12-29 12:17:21 +0700

    add: nurse entity

---
+++ features/nurses/entity.go
@@ -0,0 +1,40 @@
+package nurses
+
+import "time"
+
+type NurseCore struct {
+	ID        int
+	CreatedBy int
+	UpdatedBy int
+	Email     string
+	Password  string
+	Name      string
+	Age       int
+	ImageUrl  string
+	Phone     string
+	Address   string
+	Gender    string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type IBusiness interface {
+	FindNurses() ([]NurseCore, error)
+	FindNursesByIds(ids []int) ([]NurseCore, error)
+	FindNurseById(id int) (NurseCore, error)
+	FindNurseByEmail(email string) (NurseCore, error)
+	CreateNurse(nurse NurseCore) error
+	EditNurse(nurse NurseCore) error
+	EditNursePassword(id int, updatedBy int, oldPassword string, newPassword string) error
+	RemoveNurseById(id int, updatedBy int) error
+}
+
+type IData interface {
+	SelectNurses() ([]NurseCore, error)
+	SelectNursesByIds(ids []int) ([]NurseCore, error)
+	SelectNurseById(id int) (NurseCore, error)
+	SelectNurseByEmail(email string) (NurseCore, error)
+	InsertNurse(nurse NurseCore) error
+	UpdateNurse(nurse NurseCore) error
+	DeleteNurseById(id int, updatedBy int) error
+}
