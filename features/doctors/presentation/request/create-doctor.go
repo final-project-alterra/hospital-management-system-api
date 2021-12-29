c1ddbd020bf3ef07760b1ad922e6c61aee83ec47
rizadwiandhika <rizadwiandhika@gmail.com>
2021-12-29 09:58:49 +0700

    add: doctor related request dto

---
+++ features/doctors/presentation/request/create-doctor.go
@@ -0,0 +1,40 @@
+package request
+
+import "github.com/final-project-alterra/hospital-management-system-api/features/doctors"
+
+type CreateDoctorRequest struct {
+	CreatedBy    int
+	SpecialityID int `json:"specialityId" validate:"gt=0"`
+	RoomID       int `json:"roomId" validate:"gt=0"`
+
+	Email    string `json:"email" validate:"email"`
+	Password string `json:"password" validate:"required,min=8"`
+	Name     string `json:"name"`
+	Age      int    `json:"age"`
+	ImageUrl string `json:"imageUrl"`
+	Phone    string `json:"phone"`
+	Address  string `json:"address"`
+	Gender   string `json:"gender" validate:"oneof='L' 'P'"`
+}
+
+func (d CreateDoctorRequest) ToDoctorCore() doctors.DoctorCore {
+	return doctors.DoctorCore{
+		CreatedBy: d.CreatedBy,
+
+		Speciality: doctors.SpecialityCore{
+			ID: d.SpecialityID,
+		},
+		Room: doctors.RoomCore{
+			ID: d.RoomID,
+		},
+
+		Email:    d.Email,
+		Password: d.Password,
+		Name:     d.Name,
+		Age:      d.Age,
+		ImageUrl: d.ImageUrl,
+		Phone:    d.Phone,
+		Address:  d.Address,
+		Gender:   d.Gender,
+	}
+}
---
+++ features/doctors/presentation/request/edit-doctor.go
@@ -0,0 +1,39 @@
+package request
+
+import "github.com/final-project-alterra/hospital-management-system-api/features/doctors"
+
+type UpdateDoctorRequest struct {
+	ID        int `json:"id" validate:"gt=0"`
+	UpdatedBy int
+
+	SpecialityID int `json:"specialityId" validate:"gt=0"`
+	RoomID       int `json:"roomId" validate:"gt=0"`
+
+	Name     string `json:"name"`
+	Age      int    `json:"age"`
+	ImageUrl string `json:"imageUrl"`
+	Phone    string `json:"phone"`
+	Address  string `json:"address"`
+	Gender   string `json:"gender" validate:"oneof='L' 'P'"`
+}
+
+func (d UpdateDoctorRequest) ToDoctorCore() doctors.DoctorCore {
+	return doctors.DoctorCore{
+		ID:        d.ID,
+		UpdatedBy: d.UpdatedBy,
+
+		Speciality: doctors.SpecialityCore{
+			ID: d.SpecialityID,
+		},
+		Room: doctors.RoomCore{
+			ID: d.RoomID,
+		},
+
+		Name:     d.Name,
+		Age:      d.Age,
+		ImageUrl: d.ImageUrl,
+		Phone:    d.Phone,
+		Address:  d.Address,
+		Gender:   d.Gender,
+	}
+}
