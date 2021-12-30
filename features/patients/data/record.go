af55bf30c33c86dbe8d4612c958e001f44fec3b0
rizadwiandhika <rizadwiandhika@gmail.com>
2021-12-30 21:57:56 +0700

    add: patient record

---
+++ features/patients/data/record.go
@@ -0,0 +1,42 @@
+package data
+
+import (
+	"github.com/final-project-alterra/hospital-management-system-api/features/patients"
+	"gorm.io/gorm"
+)
+
+type Patient struct {
+	gorm.Model
+	CreatedBy int
+	UpdatedBy int
+	NIK       string `gorm:"unique_index"`
+	Name      string `gorm:"type:varchar(64)"`
+	Phone     string `gorm:"type:varchar(16)"`
+	Gender    string `gorm:"type:varchar(1)"`
+	Age       int
+	Address   string
+}
+
+func (p Patient) toPatientCore() patients.PatientCore {
+	return patients.PatientCore{
+		ID:        int(p.ID),
+		CreatedBy: p.CreatedBy,
+		UpdatedBy: p.UpdatedBy,
+		NIK:       p.NIK,
+		Name:      p.Name,
+		Age:       p.Age,
+		Phone:     p.Phone,
+		Address:   p.Address,
+		Gender:    p.Gender,
+		CreatedAt: p.CreatedAt,
+		UpdatedAt: p.UpdatedAt,
+	}
+}
+
+func toSlicePatientCore(p []Patient) []patients.PatientCore {
+	result := make([]patients.PatientCore, len(p))
+	for i := range p {
+		result[i] = p[i].toPatientCore()
+	}
+	return result
+}
