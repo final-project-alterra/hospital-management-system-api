75758af813814441a3e2f8f024177d3b4fc88118
rizadwiandhika <rizadwiandhika@gmail.com>
2022-01-02 19:15:32 +0700

    refactor: protect routes with auth middleware

--- routes/doctor.go
+++ routes/doctor.go
@@ -2,17 +2,17 @@ package routes
 
 import (
 	"github.com/final-project-alterra/hospital-management-system-api/factory"
+	"github.com/final-project-alterra/hospital-management-system-api/middleware"
 	"github.com/labstack/echo/v4"
 )
 
 func setupDoctorRoutes(e *echo.Echo, presenter *factory.Presenter) {
-	// doctor := e.Group("/doctors", middleware.IsAdmin())
 	doctor := e.Group("/doctors")
 
-	doctor.GET("", presenter.DoctorPresentation.GetDoctors)
-	doctor.GET("/:doctorId", presenter.DoctorPresentation.GetDetailDoctor)
-	doctor.POST("", presenter.DoctorPresentation.PostDoctor)
-	doctor.PUT("", presenter.DoctorPresentation.PutEditDoctor)
-	doctor.PUT("/password", presenter.DoctorPresentation.PutEditDoctorPassword)
-	doctor.DELETE("/:doctorId", presenter.DoctorPresentation.DeleteDoctor)
+	doctor.GET("", presenter.DoctorPresentation.GetDoctors, middleware.IsAuth())
+	doctor.GET("/:doctorId", presenter.DoctorPresentation.GetDetailDoctor, middleware.IsAuth())
+	doctor.POST("", presenter.DoctorPresentation.PostDoctor, middleware.IsAdmin())
+	doctor.PUT("", presenter.DoctorPresentation.PutEditDoctor, middleware.IsAdmin())
+	doctor.PUT("/password", presenter.DoctorPresentation.PutEditDoctorPassword, middleware.IsAdmin())
+	doctor.DELETE("/:doctorId", presenter.DoctorPresentation.DeleteDoctor, middleware.IsAdmin())
 }
--- routes/room.go
+++ routes/room.go
@@ -2,15 +2,15 @@ package routes
 
 import (
 	"github.com/final-project-alterra/hospital-management-system-api/factory"
+	"github.com/final-project-alterra/hospital-management-system-api/middleware"
 	"github.com/labstack/echo/v4"
 )
 
 func setupRoomRoutes(e *echo.Echo, presenter *factory.Presenter) {
-	// room := e.Group("/rooms", middleware.Isroom())
 	room := e.Group("/rooms")
 
-	room.GET("", presenter.DoctorPresentation.GetRooms)
-	room.POST("", presenter.DoctorPresentation.PostRoom)
-	room.PUT("", presenter.DoctorPresentation.PutEditRoom)
-	room.DELETE("/:roomId", presenter.DoctorPresentation.DeleteRoom)
+	room.GET("", presenter.DoctorPresentation.GetRooms, middleware.IsAuth())
+	room.POST("", presenter.DoctorPresentation.PostRoom, middleware.IsAdmin())
+	room.PUT("", presenter.DoctorPresentation.PutEditRoom, middleware.IsAdmin())
+	room.DELETE("/:roomId", presenter.DoctorPresentation.DeleteRoom, middleware.IsAdmin())
 }
