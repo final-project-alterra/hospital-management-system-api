b6ebd00cc630f08d4cbfe59261c24c224f1f3f13
rizadwiandhika <rizadwiandhika@gmail.com>
2022-01-02 19:12:36 +0700

    add: auth to factory

--- factory/factory.go
+++ factory/factory.go
@@ -14,9 +14,13 @@ import (
 	nursesBusiness "github.com/final-project-alterra/hospital-management-system-api/features/nurses/business"
 	nursesData "github.com/final-project-alterra/hospital-management-system-api/features/nurses/data"
 	nursesPresentation "github.com/final-project-alterra/hospital-management-system-api/features/nurses/presentation"
+
+	authsBusiness "github.com/final-project-alterra/hospital-management-system-api/features/auth/business"
+	authsPresentation "github.com/final-project-alterra/hospital-management-system-api/features/auth/presentation"
 )
 
 type Presenter struct {
+	AuthPresentation   *authsPresentation.AuthPresetation
 	AdminPresentation  *adminsPresentation.AdminPresentation
 	DoctorPresentation *doctorsPresentation.DoctorPresentation
 	NursePresentation  *nursesPresentation.NursePresentation
@@ -26,20 +30,28 @@ func New() *Presenter {
 	adminBuilder := adminsBusiness.NewAdminBusinessBuilder()
 	doctorBuilder := doctorsBusiness.NewDoctorBusinessBuilder()
 	nurseBuilder := nursesBusiness.NewNurseBusinessBuilder()
+	authBuilder := authsBusiness.NewAuthBusinessBuilder()
 
 	adminData := adminsData.NewMySQLRepo(config.DB)
 	doctorData := doctorsData.NewMySQLRepo(config.DB)
 	nurseData := nursesData.NewMySQLRepo(config.DB)
 
-	adminPresentation := adminsPresentation.NewAdminPresentation(adminBuilder.SetData(adminData).Build())
-	doctorPresentation := doctorsPresentation.NewDoctorPresentation(
-		doctorBuilder.SetData(doctorData).SetAdminBusiness(adminBuilder.SetData(adminData).Build()),
-	)
-	nursePresentation := nursesPresentation.NewNursePresentation(
-		nurseBuilder.SetData(nurseData).SetAdminBusiness(adminBuilder.SetData(adminData).Build()).Build(),
-	)
+	adminBusiness := adminBuilder.SetData(adminData).Build()
+	doctorBusiness := doctorBuilder.SetData(doctorData).SetAdminBusiness(adminBusiness).Build()
+	nurseBusiness := nurseBuilder.SetData(nurseData).SetAdminBusiness(adminBusiness).Build()
+	authBusiness := authBuilder.
+		SetAdminBusiness(adminBusiness).
+		SetDoctorBusiness(doctorBusiness).
+		SetNurseBusiness(nurseBusiness).
+		Build()
+
+	adminPresentation := adminsPresentation.NewAdminPresentation(adminBusiness)
+	doctorPresentation := doctorsPresentation.NewDoctorPresentation(doctorBusiness)
+	nursePresentation := nursesPresentation.NewNursePresentation(nurseBusiness)
+	authPresentation := authsPresentation.NewAuthPresentation(authBusiness)
 
 	return &Presenter{
+		AuthPresentation:   authPresentation,
 		AdminPresentation:  adminPresentation,
 		DoctorPresentation: doctorPresentation,
 		NursePresentation:  nursePresentation,
