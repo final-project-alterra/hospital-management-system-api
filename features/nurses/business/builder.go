f31b883d307e4010f430daaa4b8a37da572b15d7
rizadwiandhika <rizadwiandhika@gmail.com>
2022-01-21 00:23:35 +0700

    update: nurse create account logic

--- features/nurses/business/builder.go
+++ features/nurses/business/builder.go
@@ -2,6 +2,7 @@ package business
 
 import (
 	"github.com/final-project-alterra/hospital-management-system-api/features/admins"
+	"github.com/final-project-alterra/hospital-management-system-api/features/doctors"
 	"github.com/final-project-alterra/hospital-management-system-api/features/nurses"
 	"github.com/final-project-alterra/hospital-management-system-api/features/schedules"
 )
@@ -9,6 +10,7 @@ import (
 type nurseBusinessBuilder struct {
 	nurseRepo        nurses.IData
 	adminBusiness    admins.IBusiness
+	doctorBusiness   doctors.IBusiness
 	scheduleBusiness schedules.IBusiness
 }
 
@@ -20,11 +22,13 @@ func (n *nurseBusinessBuilder) Build() nurses.IBusiness {
 	nurseBusiness := &nurseBusiness{
 		data:             n.nurseRepo,
 		adminBusiness:    n.adminBusiness,
+		doctorBusiness:   n.doctorBusiness,
 		scheduleBusiness: n.scheduleBusiness,
 	}
 
 	n.nurseRepo = nil
 	n.adminBusiness = nil
+	n.doctorBusiness = nil
 	n.scheduleBusiness = nil
 
 	return nurseBusiness
@@ -37,6 +41,11 @@ func (n *nurseBusinessBuilder) SetData(data nurses.IData) *nurseBusinessBuilder 
 
 func (n *nurseBusinessBuilder) SetAdminBusiness(adminBusiness admins.IBusiness) *nurseBusinessBuilder {
 	n.adminBusiness = adminBusiness
+	return n
+}
+
+func (n *nurseBusinessBuilder) SetDoctorBusiness(doctorBusiness doctors.IBusiness) *nurseBusinessBuilder {
+	n.doctorBusiness = doctorBusiness
 	return n
 }
 
