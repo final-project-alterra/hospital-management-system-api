d523f255ac77708e9aa8eb5c437fa851f2ee975b
rizadwiandhika <rizadwiandhika@gmail.com>
2022-01-21 00:23:11 +0700

    update: admin create account logic

--- features/admins/business/builder.go
+++ features/admins/business/builder.go
@@ -1,9 +1,15 @@
 package business
 
-import "github.com/final-project-alterra/hospital-management-system-api/features/admins"
+import (
+	"github.com/final-project-alterra/hospital-management-system-api/features/admins"
+	"github.com/final-project-alterra/hospital-management-system-api/features/doctors"
+	"github.com/final-project-alterra/hospital-management-system-api/features/nurses"
+)
 
 type adminBusinessBuilder struct {
-	adminRepo admins.IData
+	adminRepo      admins.IData
+	doctorBusiness doctors.IBusiness
+	nurseBusiness  nurses.IBusiness
 }
 
 func NewAdminBusinessBuilder() *adminBusinessBuilder {
@@ -14,13 +20,25 @@ func (b *adminBusinessBuilder) SetData(data admins.IData) *adminBusinessBuilder 
 	b.adminRepo = data
 	return b
 }
+func (b *adminBusinessBuilder) SetDoctorBusiness(db doctors.IBusiness) *adminBusinessBuilder {
+	b.doctorBusiness = db
+	return b
+}
+func (b *adminBusinessBuilder) SetNurseBusiness(nb nurses.IBusiness) *adminBusinessBuilder {
+	b.nurseBusiness = nb
+	return b
+}
 
 func (b *adminBusinessBuilder) Build() admins.IBusiness {
 	adminBusiness := &adminBusiness{
-		data: b.adminRepo,
+		data:           b.adminRepo,
+		doctorBusiness: b.doctorBusiness,
+		nurseBusiness:  b.nurseBusiness,
 	}
 
 	b.adminRepo = nil
+	b.doctorBusiness = nil
+	b.nurseBusiness = nil
 
 	return adminBusiness
 }
