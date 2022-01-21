fbdc34175dd3d6795e1d80af9ecbecfebf94fa31
rizadwiandhika <rizadwiandhika@gmail.com>
2022-01-21 17:10:50 +0700

    add: route setup for nurse image profile

--- routes/nurse.go
+++ routes/nurse.go
@@ -14,7 +14,9 @@ func setupNurseRoutes(e *echo.Echo, presenter *factory.Presenter) {
 	nurses.POST("", presenter.NursePresentation.PostNurse, middleware.IsAdmin())
 	nurses.PUT("", presenter.NursePresentation.PutEditNurse, middleware.IsAdmin())
 	nurses.PUT("/password", presenter.NursePresentation.PutEditNursePassword, middleware.IsAdmin())
+	nurses.PUT("/image-profile", presenter.NursePresentation.PutEditImageProfile, middleware.IsAdmin())
 	nurses.DELETE("/:nurseId", presenter.NursePresentation.DeleteNurse, middleware.IsAdmin())
+	nurses.DELETE("/:nurseId/image-profile", presenter.NursePresentation.DeleteImageProfile, middleware.IsAdmin())
 
 	nurses.GET("/:nurseId/work-schedules", presenter.SchedulePresentation.GetNurseWorkSchedules, middleware.IsAuth())
 }
